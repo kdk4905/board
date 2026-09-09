210bd831cb7f58e4326fa55bb5e1ff1967f2cf8e
Hyesung1122 <154863030+Hyesung1122@users.noreply.github.com>
2026-09-09 18:56:01 +0900

    [YHS TEST]
    브랜치 연결 테스트

--- board/src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ board/src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
 import java.util.List;
-
+//유해성 0909
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
